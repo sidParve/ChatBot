fd8e359ee621d46f2301acd418eb0e4b12254008
govind-kande <govind.j.kande@gmail.com>
2023-08-14 01:25:12 +0530

    Added Streaming functionality v1.2 - 14/08/2023 : 01:19

--- ChatGptApp/src/main/java/com/chatgpt/controller/RequestController.java
+++ ChatGptApp/src/main/java/com/chatgpt/controller/RequestController.java
@@ -2,13 +2,11 @@ package com.chatgpt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import com.chatgpt.model.Request;
 import com.chatgpt.model.Response;
@@ -26,10 +24,15 @@ public class RequestController {
 	RestTemplate restTemplate;
 
 	@GetMapping("/chat")
-	public String getResponse(@RequestParam("content") String content) {
+	public StreamingResponseBody  getResponse(@RequestParam("content") String content) {
 		Request request = new Request(model, content);
 		Response response = restTemplate.postForObject(URL, request, Response.class);
-
-		return response.getChoices().get(0).getMessage().getContent();
+		
+		 return outputStream -> {
+	            // Write your API response data to the output stream
+	            // You can read data from your API and write it to the output stream in chunks
+	            // Example: Read data from an API response and write it to the output stream
+	            outputStream.write(response.getChoices().get(0).getMessage().getContent().getBytes());
+	        };
 	}
 }
